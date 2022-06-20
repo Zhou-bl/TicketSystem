7f3fada80c0ce72c9f39770d92513fb3e2839fae
Zhou_bl <zhoubinglin2002@163.com>
2022-06-20 08:11:16 +0800

    first

--- main.cpp
+++ main.cpp
@@ -5,8 +5,8 @@
 CommandParser ticketSystem;
 
 int main() {
-    freopen("testdata/basic_3/5.in", "r", stdin);
-    freopen("ans.txt", "w", stdout);
+    //freopen("testdata/basic_3/5.in", "r", stdin);
+    //freopen("ans.txt", "w", stdout);
     std::string commandLine, result;
     while (std::getline(std::cin, commandLine)) {
         result = ticketSystem.run(commandLine);
